888479edb2f0484da4113eed4f0be9bca1b24a86
fuwaki <tiny.fly@outlook.com>
2026-05-29 12:58:21 +0800

    ci: 增加github actions到pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
+  base: process.env.BASE_PATH ?? '/',
   plugins: [
     vue(),
     vueDevTools(),
